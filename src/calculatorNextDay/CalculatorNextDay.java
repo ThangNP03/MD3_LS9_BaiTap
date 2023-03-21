1f46605cbdc60aff9735f7262a616c96e6d92f64
ThangNP03 <2003nguyenphucthang@gmail.com>
2023-03-21 08:27:08 +0700

    BAiTap

---
+++ src/calculatorNextDay/CalculatorNextDay.java
@@ -0,0 +1,35 @@
+package calculatorNextDay;
+
+public class CalculatorNextDay {
+    public static String nextDay(int day, int month, int year) {
+        int newDay = 0;
+        int newMonth = 0;
+        int newYear = 0;
+        if (day == 1) {
+            newDay = day + 1;
+            newMonth = month;
+            newYear = year;
+            return newDay + "/" + newMonth + "/" + newYear;
+        }
+        if (day == 30 || day == 28 || day == 29) {
+            day = 1;
+            newDay = day;
+            newMonth = month + 1;
+            newYear = year;
+            return newDay + "/" + newMonth + "/" + newYear;
+        } else if (day == 31 && month == 12) {
+            newDay = 1;
+            newMonth = 1;
+            newYear = year + 1;
+            return newDay + "/" + newMonth + "/" + newYear;
+        } else if (day == 31 && month != 12) {
+
+            day = 1;
+            newDay = day;
+            newMonth = month + 1;
+            newYear = year;
+            return newDay + "/" + newMonth + "/" + newYear;
+        }
+        return newDay + "/" + newMonth + "/" + newYear;
+    }
+}
